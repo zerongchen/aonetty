ebbc850c6b5243ca962ccea5790a53d86c93e3ea
zerongchen <zerongchen@users.noreply.github.com>
2018-07-06 17:06:00 +0800

    fix

--- src/main/java/com/netty/aonet/nty/ChildChannelHandler.java
+++ src/main/java/com/netty/aonet/nty/ChildChannelHandler.java
@@ -14,8 +14,9 @@ public class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
         //LineBasedFrameDecoder && StringDecoder 主要用于TCP粘包问题
         /**
          * LineBasedFrameDecoder 一次遍历byteBuf中的/n /r/n 如果有这视为结束位置，否者超过最大长度后抛出异常，且忽略之前读到的字节
-         *
+         * DelimiterBasedFrameDecoder 自定义字符分割TCP内容，防止粘包
          */
+//        new DelimiterBasedFrameDecoder(1024, Unpooled.copiedBuffer("$".getBytes()));
         socketChannel.pipeline().addLast(new LineBasedFrameDecoder(1024));
         socketChannel.pipeline().addLast(new StringDecoder());
         socketChannel.pipeline().addLast(new TimeServerHandler());
--- src/main/java/com/netty/aonet/nty/TimeServer.java
+++ src/main/java/com/netty/aonet/nty/TimeServer.java
@@ -6,6 +6,8 @@ import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 
 public class TimeServer {
 
@@ -28,6 +30,7 @@ public class TimeServer {
         b.group(boosGroup,workerGroup)
                 .channel(NioServerSocketChannel.class)
                 .option(ChannelOption.SO_BACKLOG,1024)
+                .handler(new LoggingHandler(LogLevel.DEBUG))
                 .childHandler(new ChildChannelHandler());
         //绑定端口，同步等待成功
 
